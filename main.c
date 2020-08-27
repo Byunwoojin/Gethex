e9521f957402e857daa0b213bde6b673e0db413a
Byunwoojin <sforot@naver.com>
2020-08-27 13:10:58 +0900

    Add C Source File [main.c]

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h> 
+#include <ctype.h> 
+#define SIZE 100
+
+main()
+{
+	int array[SIZE], n;
+	int gethex(int *);
+	int i, sum;
+
+	for (n = 0; n < SIZE && gethex(&array[n]) != EOF; n++)
+		;
+	sum = 0;
+	for (i = 0; i < n; i++) {
+		printf("array[%d]: %d\n",i, array[i]);
+	}
+	for (i = 0; i < n; i++) {
+		sum += array[i];
+	}
+	printf("The sum is %d\n", sum);
+}
+
+/* getint:  get next integer from input into *pn */ 
+int gethex(int *pn) {
+	int c, sign;
+
+	while (isspace(c = getchar()))   
+		;
+	if (!isxdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetc(c, stdin);  
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-')
+		c = getchar();
+	for (*pn = 0; isxdigit(c); c = getchar()) {
+		if (c >= '0'&&c <= '9') {
+			*pn = 16 * *pn + (c - '0');
+		}
+		else  {
+			c=tolower(c);
+			*pn = 16 * *pn + (c - 'a' + 10);
+		}
+	}
+	*pn *= sign;
+		
+
+		
+	if (c != EOF)
+		ungetc(c, stdin);
+	return c;
+}
